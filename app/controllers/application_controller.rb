e94d0bf44f0407f5b6cdfceabdcc34c99ba37d3f
Jordan Hall <jordanhall.1995@yahoo.com>
2017-04-27 10:03:58 -0700

    User can only access content on their page and or what they add somewhere else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :authorize
+  helper_method :current_user, :logged_in?, :authorize, :user_allowed
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -12,5 +12,9 @@ class ApplicationController < ActionController::Base
 
   def authorize
     redirect_to new_home_path unless logged_in?
+  end
+
+  def user_allowed
+    logged_in? && (current_user.id == @comment.user_id) || (current_user.id == @post.user_id) || (current_user.id == @user.id)
   end
 end
